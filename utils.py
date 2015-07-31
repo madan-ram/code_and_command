076b2c1f8e31d87f5dc6af35781b0453f174c181
madan-ram <madan_ram@rocketmail.com>
2015-07-31 12:32:15 +0530

    window generator from specified location

--- utils.py
+++ utils.py
@@ -1,10 +1,56 @@
 # -*- coding: utf-8 -*-
+import cv2
 import numpy as np
 import os
 from os.path import isfile, join
 from os import listdir
-import cv2
+import math
+import sys
+import itertools
 
+def generate_window_locations(location, patch_shape, stride=0.5, grid_shape=5):
+	"""
+		Generate a list of window location based on location, patch_shape, grid_shape and stride
+
+		location:
+			(y, x) a tuple for center location of window.
+		patch_shape:
+			(y, x) a tuple which represent window shape.
+		stride {default: 0.5}:
+			a float which represent stride to be taken.
+		grid_shape :
+			a integer that represent grid shape where grid_x == grid_y
+
+	"""
+	assert(grid_shape%2 != 0), "grid_shape should be odd number"
+
+	assert(stride != 0), "stride should not be <= 0"
+
+	center_y, center_x = location
+	string = ""
+	mapping = {}
+
+	# left is represented as -, center as 0  and right is +
+	if grid_shape%2 != 0:
+		pointer = xrange(-(grid_shape/2), (grid_shape/2)+1)
+	# else:
+	# 	pointer = range(-(grid_shape/2)+1, (grid_shape/2)+1)
+
+	for i, n, in enumerate(pointer):
+		mapping[i] = n
+		string += str(i)
+	windows_list = []	
+	sequences = np.asarray(list(itertools.product(string, repeat=2)), dtype="int32")
+
+	for y, x in sequences:
+		new_center_y, new_center_x = center_y +patch_shape[0]*mapping[y]*stride, center_x+patch_shape[1]*mapping[x]*stride
+
+		res =  np.asarray([(math.floor(new_center_y)-patch_shape[0]/2,math.floor(new_center_x)-patch_shape[1]/2), 
+		(math.ceil(new_center_y)+patch_shape[0]/2, math.ceil(new_center_x)+patch_shape[1]/2)], dtype="int32")
+
+		windows_list.append(res)
+
+	return np.asarray(windows_list).tolist()
 
 def getImmediateSubdirectories(dir):
 	"""
@@ -19,67 +65,6 @@ def getImmediateSubdirectories(dir):
 	"""
 
 	return [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]
-
-def create_fixed_image_shape(img, frame_size=(200, 200, 3), random_fill=True, mode='crop'):
-	image_frame = None
-	if mode == 'fit':
-		X1, Y1, _ = frame_size
-		if random_fill:
-			image_frame = np.asarray(np.random.randint(0, high=255, size=frame_size), dtype='uint8')
-			print image_frame.shape
-		else:
-			image_frame = np.zeros(frame_size, dtype='uint8')
-
-		X2, Y2 = img.shape[1], img.shape[0]
-
-		if X2 > Y2:
-			X_new = X1
-			Y_new = int(round(float(Y2*X_new)/float(X2)))
-		else:
-			Y_new = Y1
-			X_new = int(round(float(X2*Y_new)/float(Y2)))
-
-		img = cv2.resize(img, (X_new, Y_new))
-
-		X_space_center = ((X1 - X_new)/2)
-		Y_space_center = ((Y1 - Y_new)/2)
-
-		# print Y_new, X_new, Y_space_center, X_space_center
-		image_frame[Y_space_center: Y_space_center+Y_new, X_space_center: X_space_center+X_new, :] = img
-		
-	elif mode == 'crop':
-		X1, Y1, _ = frame_size
-		image_frame = np.zeros(frame_size, dtype='uint8')
-
-		X2, Y2 = img.shape[1], img.shape[0]
-
-		#increase the size of smaller length (width or hegiht)
-		if X2 > Y2:
-			Y_new = Y1
-			X_new = int(round(float(X2*Y_new)/float(Y2)))
-		else:
-			X_new = X1
-			Y_new = int(round(float(Y2*X_new)/float(X2)))
-
-		img = cv2.resize(img, (X_new, Y_new))
-
-		
-		X_space_clip = (X_new - X1)/2
-		Y_space_clip = (Y_new - Y1)/2
-
-		#trim image both top, down, left and right
-		if X_space_clip == 0 and Y_space_clip != 0:
-			img = img[Y_space_clip:-Y_space_clip, :]
-		elif Y_space_clip == 0 and X_space_clip != 0:
-			img = img[:, X_space_clip:-X_space_clip]
-
-		if img.shape[0] != X1:
-			img = img[1:, :]
-		if img.shape[1] != Y1:
-			img = img[:, 1:]
-
-		image_frame[: , :] = img
-	return image_frame
 
 def getFiles(dir_path):
     """getFiles : gets the file in specified directory
@@ -122,11 +107,8 @@ def feature_normalization(data, type='standardization', params = None):
 	"""
 	if type == 'standardization':
 		if params is None:
-			params = {}
 			mu = np.mean(data, axis=0)
 			sigma =  np.std(data, axis=0)
-			params['mu'] = mu
-			params['sigma'] = sigma
 		else:
 			mu = params['mu']
 			sigma = params['sigma']
@@ -135,11 +117,8 @@ def feature_normalization(data, type='standardization', params = None):
 
 	elif type == 'min-max':
 		if params is None:
-			params = {}
 			Xmin = np.min(data, axis=0)
 			Xmax = np.max(data, axis=0)
-			params['Xmin'] = Xmin
-			params['Xmax'] = Xmax
 		else:
 			Xmin = params['Xmin']
 			Xmax = params['Xmax']
@@ -147,4 +126,4 @@ def feature_normalization(data, type='standardization', params = None):
 		Xmax = Xmax.astype('float')
 		Xmin = Xmin.astype('float')
 		Z = (data - Xmin)/(Xmax - Xmin)
-		return Z, params
+		return Z, Xmax, Xmin
