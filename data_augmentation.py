8078f321e2aa701651b000f8949960bd7bda54a1
madan-ram <madan_ram@rocketmail.com>
2015-10-27 12:27:01 +0530

    modified data agumentation

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,40 +1,39 @@
 import cv2
 import numpy as np
 from utils import *
-import sys
 from scipy.ndimage.interpolation import rotate
 
-def data_augmentation(orignial_img, size_factor_up=1.5, size_factor_down=0.5):
+def data_augmentation(orignial_img, size_factor_up=1.25, size_factor_down=0.5):
 	result = []
 	img = create_fixed_image_shape(orignial_img, frame_size=(256, 256, 3), random_fill=True, mode='fit')
 	result.append(img)
 	img = create_fixed_image_shape(orignial_img, frame_size=(256, 256, 3), random_fill=True, mode='crop')
 	result.append(img)
 
-	#rotate image 30 45 -30 -45
-	img_30 = rotate(orignial_img, 30)
+	#rotate image 30 45 -30 -45, or random rotation
+	img_30 = rotate(orignial_img, np.random.randint(10, high=30))
 	img = create_fixed_image_shape(img_30, frame_size=(256, 256, 3), random_fill=True, mode='fit')
 	result.append(img)
-	img = create_fixed_image_shape(img_30, frame_size=(256, 256, 3), random_fill=True, mode='crop')
-	result.append(img)
+	# img = create_fixed_image_shape(img_30, frame_size=(256, 256, 3), random_fill=True, mode='crop')
+	# result.append(img)
 
-	img_45 = rotate(orignial_img, 45)
+	img_45 = rotate(orignial_img, np.random.randint(30, high=45))
 	img = create_fixed_image_shape(img_45, frame_size=(256, 256, 3), random_fill=True, mode='fit')
 	result.append(img)
-	img = create_fixed_image_shape(img_45, frame_size=(256, 256, 3), random_fill=True, mode='crop')
-	result.append(img)
+	# img = create_fixed_image_shape(img_45, frame_size=(256, 256, 3), random_fill=True, mode='crop')
+	# result.append(img)
 
-	img_m30 = rotate(orignial_img, -30)
+	img_m30 = rotate(orignial_img, np.random.randint(-30, high=-10))
 	img = create_fixed_image_shape(img_m30, frame_size=(256, 256, 3), random_fill=True, mode='fit')
 	result.append(img)
-	img = create_fixed_image_shape(img_m30, frame_size=(256, 256, 3), random_fill=True, mode='crop')
-	result.append(img)
+	# img = create_fixed_image_shape(img_m30, frame_size=(256, 256, 3), random_fill=True, mode='crop')
+	# result.append(img)
 
-	img_m45 = rotate(orignial_img, -45)
+	img_m45 = rotate(orignial_img, np.random.randint(-45, high=-30))
 	img = create_fixed_image_shape(img_m45, frame_size=(256, 256, 3), random_fill=True, mode='fit')
 	result.append(img)
-	img = create_fixed_image_shape(img_m45, frame_size=(256, 256, 3), random_fill=True, mode='crop')
-	result.append(img)
+	# img = create_fixed_image_shape(img_m45, frame_size=(256, 256, 3), random_fill=True, mode='crop')
+	# result.append(img)
 
 	#scale up by factor of 50%(size_factor_up) more
 	img_large = cv2.resize(orignial_img, (int(orignial_img.shape[1]*size_factor_up),int(orignial_img.shape[0]*size_factor_up)))
@@ -48,9 +47,6 @@ def data_augmentation(orignial_img, size_factor_up=1.5, size_factor_down=0.5):
 
 	img = create_fixed_image_shape(img_large, frame_size=(256, 256, 3), random_fill=True, mode='crop')
 	result.append(img)
-
-	cv2.imshow('img_large', img)
-	cv2.waitKey(0)
 
 	#scale down by factor of 50%(size_factor_down) more
 	img_small = cv2.resize(orignial_img, (int(orignial_img.shape[1]*size_factor_down),int(orignial_img.shape[0]*size_factor_down)))
@@ -70,11 +66,3 @@ def data_augmentation(orignial_img, size_factor_up=1.5, size_factor_down=0.5):
 	result.append(img)
 
 	return result
-
-
-# for f in getFiles(sys.argv[1]):
-# 	path_files = sys.argv[1]+'/'+f
-# 	img = cv2.imread(path_files)
-# 	result = data_augmentation(img, size_factor_up=1.5)
-
-# 	print len(result)
